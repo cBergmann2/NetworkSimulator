bd4ad180ec334ccd98644b82b129c1bb0f041eb6
Christoph Bergmann <c.bergmann2@gmx.net>
2017-05-07 08:59:53 +0200

    TC-Nachricht implementiert

--- src/TC_Nachricht.java
+++ src/TC_Nachricht.java
@@ -1,4 +1,4 @@
-
+import java.util.LinkedList;
 
 /**
  * @author Christoph Bergmann
@@ -6,9 +6,27 @@
  * @created 05-Mai-2017 11:06:30
  */
 public class TC_Nachricht {
+	
+	private int originatorID;
+	private LinkedList<Integer> advertisedNeighbors;
 
-	public TC_Nachricht(){
-
+	public TC_Nachricht(int originatorID){
+		this.originatorID = originatorID;
+		this.advertisedNeighbors = new LinkedList<Integer>();
+	}
+	
+	public void addAdvertisedNeighbor(int advertisedNeighborID){
+		if(!advertisedNeighbors.contains(advertisedNeighborID)){
+			advertisedNeighbors.add(advertisedNeighborID);
+		}
+	}
+	
+	public int getOriginatorID(){
+		return this.originatorID;
+	}
+	
+	public LinkedList<Integer> getAdvertisedNeighbors(){
+		return this.advertisedNeighbors;
 	}
 
 	public void finalize() throws Throwable {
